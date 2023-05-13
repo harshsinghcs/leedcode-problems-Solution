b4ab89c79839ef5d1d16e912d19558c3ce189df8
Harsh Singh <115187902+harshsinghcs@users.noreply.github.com>
2023-05-13 23:41:08 +0530

    Create 2466. Count Ways To Build Good Strings.java

---
+++ 2466. Count Ways To Build Good Strings.java
@@ -0,0 +1,61 @@
+/*
+
+2466. Count Ways To Build Good Strings 
+all code in java by harshsinghcs
+
+Question👇👇
+
+Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
+
+Append the character '0' zero times.
+Append the character '1' one times.
+This can be performed any number of times.
+
+A good string is a string constructed by the above process having a length between low and high (inclusive).
+
+Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: low = 3, high = 3, zero = 1, one = 1
+Output: 8
+Explanation: 
+One possible valid good string is "011". 
+It can be constructed as follows: "" -> "0" -> "01" -> "011". 
+All binary strings from "000" to "111" are good strings in this example.
+Example 2:
+
+Input: low = 2, high = 3, zero = 1, one = 2
+Output: 5
+Explanation: The good strings are "00", "11", "000", "110", and "011".
+ 
+
+Constraints:
+
+1 <= low <= high <= 105
+1 <= zero, one <= low
+
+*/
+
+class Solution {
+  public int countGoodStrings(int low, int high, int zero, int one) {
+    final int kMod = 1_000_000_007;
+    int ans = 0;
+    // dp[i] := # of good strings with length i
+    int[] dp = new int[high + 1];
+    dp[0] = 1;
+
+    for (int i = 1; i <= high; ++i) {
+      if (i >= zero)
+        dp[i] = (dp[i] + dp[i - zero]) % kMod;
+      if (i >= one)
+        dp[i] = (dp[i] + dp[i - one]) % kMod;
+      if (i >= low)
+        ans = (ans + dp[i]) % kMod;
+    }
+
+    return ans;
+  }
+}
